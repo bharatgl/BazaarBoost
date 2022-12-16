1649338049ceae44ace7462a6543339b8bdcb12c
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2022-12-17 02:53:43 +0530

    feat: building Header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Layout from "./components/Layout";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+
+import "./App.css";
 function App() {
   return (
     <>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                free shipping over $100 & Returns
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline:
+                <a href="tel:+919551121295" className="text-white">
+                  +91 9551121295
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-2">
+              <h2>
+                <Link className="text-white">BazaarBoost</Link>
+              </h2>
+            </div>
+            <div className="col-5"></div>
+            <div className="col-5"></div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
