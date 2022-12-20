dc1f9caf0acc5176d30135c3f7370703cdeb4e1a
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2022-12-20 17:24:19 +0530

    completed the footer section✅

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BsLinkedin, BsGithub, BsYoutube, BsInstagram } from "react-icons/bs";
 
 const Footer = () => {
   return (
@@ -23,14 +25,83 @@ const Footer = () => {
                   aria-describedby="basic-addon2"
                 />
                 <span className="input-group-text p-2" id="basic-addon2">
-                 Subscribe
+                  Subscribe
                 </span>
               </div>
             </div>
           </div>
         </div>
       </footer>
-      <footer className="py-3"></footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h4 className="text-white  mb-4">Contact Us</h4>
+              <div>
+                <address className="text-white fs-6">
+                  h. no. 4, Near Sai Mandir,
+                  <br /> Ludhiana, Punjab <br />
+                  Pincode: 141013
+                </address>
+                <a
+                  href="tel:+91 8475522698"
+                  className="mt-3 d-block mb-1 text-white"
+                >
+                  tel : +91 8475522698
+                </a>
+                <a
+                  href="mailto:info@BazaarBoost.com"
+                  className="mt-3 d-block mb-0 text-white"
+                >
+                  mailto : info@BazaarBoost.com
+                </a>
+                <div className="socila-icons d-flex align-items-center gap-30 mt-4">
+                  <a className="text-white" href="https://www.linkedin.com/in/bharatgl/">
+                    <BsLinkedin className="fs-4"/>
+                  </a>
+                  <a className="text-white" href="https://www.instagram.com/thelifeofbharatgl">
+                    <BsInstagram className="fs-4"/>
+                  </a>
+                  <a className="text-white" href="https://www.youtube.com/@bharatgl">
+                    <BsYoutube className="fs-4"/>
+                  </a>
+                  <a className="text-white" href="https://github.com/bharatgl">
+                    <BsGithub className="fs-4"/>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white  mb-4">Information</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Privacy Policy</Link>
+                <Link className="text-white py-2 mb-1">Refund Policy</Link>
+                <Link className="text-white py-2 mb-1">Shipping Policy</Link>
+                <Link className="text-white py-2 mb-1">Terms & Conditions</Link>
+                <Link className="text-white py-2 mb-1">Blogs</Link>
+              </div>{" "}
+              <div></div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white  mb-4">Account</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Abouts</Link>
+                <Link className="text-white py-2 mb-1">Faq</Link>
+                <Link className="text-white py-2 mb-1">Contact</Link>
+              </div>
+            </div>
+            <div className="col-2">
+              <h4 className="text-white  mb-4">Quick Links</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Laptops</Link>
+                <Link className="text-white py-2 mb-1">Headphones</Link>
+                <Link className="text-white py-2 mb-1">Tablets</Link>
+                <Link className="text-white py-2 mb-1">Watch</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
       <footer className="py-4">
         <div className="container-xxl">
           <div className="row">
