1395703cfd4b6131a7e18bce0994f9ae3ba234fd
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2023-01-06 04:10:55 +0530

    Feat: featured_collection_card completed

---
+++ src/components/ProductCard.js
@@ -0,0 +1,59 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+import { Link } from "react-router-dom";
+
+const ProductCard = () => {
+  return (
+    <div className="col-3">
+      <Link className="product-card position-relative">
+        <div className="wishlist-icon position-absolute">
+          <Link>
+            <img src="images/wish.svg" alt="wishlist" />
+          </Link>
+        </div>
+
+        <div className="product-image">
+          <img src="images/watch.jpg" className="img-fluid" alt="product img" />
+          <img
+            src="images/watch-1.webp"
+            className="img-fluid"
+            alt="product img"
+          />
+        </div>
+        <div className="product-details">
+          <h6 className="brand">Havels</h6>
+          <h5 className="product-title">
+            Kids headphones bulk 10 pack multi colored for student
+          </h5>
+          <ReactStars
+            count={5}
+            size={24}
+            value="3"
+            edit={false}
+            isHalf={true}
+            emptyIcon={<i className="far fa-star"></i>}
+            halfIcon={<i className="fa fa-star-half-alt"></i>}
+            fullIcon={<i className="fa fa-star"></i>}
+            activeColor="#ffd700"
+          />
+          <p className="price">$100.00</p>
+        </div>
+        <div className="action-bar position-absolute">
+          <div className="d-flex flex-column gap-15">
+            <Link>
+              <img src="images/prodcompare.svg" alt="compare" />
+            </Link>
+            <Link>
+              <img src="images/view.svg" alt="view" />
+            </Link>
+            <Link>
+              <img src="images/add-cart.svg" alt="addcart" />
+            </Link>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Marquee from "react-fast-marquee";
+import BlogCard from "../components/BlogCard";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
   return (
@@ -204,28 +206,70 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className="marquee-wrapper py-5">
-        <div className="cotainer-xxl">
+      <section className="featured-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <h3 className="section-heading">Featured Collections</h3>
+            </div>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
+        </div>
+      </section>
+      <section className="marquee-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
           <div className="row">
             <div className="col-12">
               <div className="marquee-inner-wrapper card-wrapper">
                 <Marquee className="d-flex">
-<div className="mx-4 w-25"><img src="images/brand-01.png" alt="brand" /></div>                
-<div className="mx-4 w-25"><img src="images/brand-02.png" alt="brand" /></div>                
-<div className="mx-4 w-25"><img src="images/brand-03.png" alt="brand" /></div>                
-<div className="mx-4 w-25"><img src="images/brand-04.png" alt="brand" /></div>                
-<div className="mx-4 w-25"><img src="images/brand-05.png" alt="brand" /></div>                
-<div className="mx-4 w-25"><img src="images/brand-06.png" alt="brand" /></div>                
-<div className="mx-4 w-25"><img src="images/brand-07.png" alt="brand" /></div>                
-<div className="mx-4 w-25"><img src="images/brand-08.png" alt="brand" /></div>                
-</Marquee>
+                  <div className="mx-4 w-25">
+                    <img src="images/brand-01.png" alt="brand" />
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src="images/brand-02.png" alt="brand" />
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src="images/brand-03.png" alt="brand" />
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src="images/brand-04.png" alt="brand" />
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src="images/brand-05.png" alt="brand" />
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src="images/brand-06.png" alt="brand" />
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src="images/brand-07.png" alt="brand" />
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src="images/brand-08.png" alt="brand" />
+                  </div>
+                </Marquee>
               </div>
             </div>
           </div>
         </div>
       </section>
+      <section className="blog-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <h3 className="section-heading">Our Latest Blogs</h3>
+            </div>
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+          </div>
+        </div>
+      </section>
     </>
-  ); 
+  );
 };
 
 export default Home;
