ac7aa2deadee2e83dd51a8d4c144d68c45d5d718
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2023-01-12 02:04:16 +0530

    feat : created card and sortby option

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactStars from "react-rating-stars-component";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
+import ProductCard from "../components/ProductCard";
 
 const OurStore = () => {
+  const [grid, setGird] = useState(4);
+
+  const gridSetter = (i) => {
+    setGird(i);
+  };
+  alert(grid);
+
   return (
     <>
       <Meta title={"Our Store"} />
@@ -227,7 +235,81 @@ const OurStore = () => {
                 </div>
               </div>
             </div>
-            <div className="col-9"></div>
+            <div className="col-9">
+              <div className="filter-sort-grid mb-4">
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex alig-items-center gap-10">
+                    <p className="mb-0 d-block " style={{ width: "100px" }}>
+                      Sort By:
+                    </p>
+                    <select name="" id="" className="form-control form-select">
+                      <option value="manual">Featured</option>
+                      <option value="best-selling" selected="selected">
+                        Best Selling
+                      </option>
+                      <option value="title-ascending">
+                        Alphabetically, A-Z
+                      </option>
+                      <option value="title-descending">
+                        Alphabetically, A-Z
+                      </option>
+                      <option value="price-ascending">
+                        Price, low to high
+                      </option>
+                      <option value="price-descending">
+                        Price, high to low
+                      </option>
+                      <option value="created-descending">
+                        Date, old to new
+                      </option>
+                      <option value="created-descending">
+                        Date, new to old
+                      </option>
+                    </select>
+                  </div>
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="totalproducts mb-0">21 Products</p>
+                    <div className="d-flex gap-10 align-items-center grid">
+                      <img
+                        onClick={() => {
+                          setGird(4);
+                        }}
+                        src="images/gr4.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGird(3);
+                        }}
+                        src="images/gr3.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGird(2);
+                        }}
+                        src="images/gr2.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGird(1);
+                        }}
+                        src="images/gr.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="products-list pb-5">
+                <ProductCard grid={grid} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
